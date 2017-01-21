793f701dacab3e2ddcbd1cac9fe959dc9a5b7675
Yusuf Kibaya <yusuf.kibaya@gmail.com>
2017-01-21 18:09:56 +0300

    Setup webpack with css-loader, file-loader, html-webpack-plugin, lost, postcss-loader, rupture, style-loader, stylus and stylus-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,52 @@
+var path =  require ('path');
+var rupture = require('rupture');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
-    path: './dist'
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+  	rules: [
+  		{
+  			test: /\.css$/,
+  			use: [
+  				'style-loader',
+  				'css-loader?modules&importLoaders=1',
+  				'postcss-loader'
+  			]
+  		},
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1',
+          'postcss-loader',
+          {
+            loader: 'stylus-loader',
+            options: {
+              use: rupture(),
+            },
+        },
+        ]
+      },
+  	]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'landing.html',
+      template: 'landing.html',
+      inject: 'body',
+    })
+  ]
+  /*,
+  stylus: {
+    use: [rupture()]
   }
+  plugins: [
+  	require('autoprefixer')
+  ]*/
 }
